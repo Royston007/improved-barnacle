d9648547f1ccec1bf847b5686e95c31fcc24633b
Royston007 <roystoncheck12@gmail.com>
2024-01-30 15:11:23 +0530

    Update InsertFarmAnimal.py

--- InsertFarmAnimal.py
+++ InsertFarmAnimal.py
@@ -5,10 +5,16 @@ import uuid
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb',region_name = 'us-east-1')
     table = dynamodb.Table('FarmAnimal')
+    #print('event ' , json.loads(event["body"]))
+    ani = ''
+    if 'body'  in event:
+        ani = json.loads(event["body"])
+    else:
+        ani = event
     response = table.put_item(
         Item = {
             "GGID" : str(uuid.uuid1()),
-            "Animal_Type" :  event['animal']
+            "Animal_Type" :  ani['animal']
             }
         )
     return response
