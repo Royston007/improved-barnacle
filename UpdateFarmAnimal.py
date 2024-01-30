e2460be8c5673eb732413b360884199b0a9b2bd6
Royston007 <roystoncheck12@gmail.com>
2024-01-30 15:11:49 +0530

    Update UpdateFarmAnimal.py

--- UpdateFarmAnimal.py
+++ UpdateFarmAnimal.py
@@ -7,29 +7,34 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb',region_name = 'us-east-1')
     table = dynamodb.Table('FarmAnimal')
     
+    ani = ''
+    if 'body'  in event:
+        ani = json.loads(event["body"])
+    else:
+        ani = event
     
     get_obj = table.query(
-        KeyConditionExpression=Key('GGID').eq(event['uuid'])
+        KeyConditionExpression=Key('GGID').eq(ani['uuid'])
     )
     print(get_obj)
     #return get_obj['Items'][0]['Animal_Type']
     table.delete_item(
         Key = {
-            "GGID" : event['uuid'],
+            "GGID" : ani['uuid'],
             "Animal_Type" : get_obj['Items'][0]['Animal_Type']
         }
         )
-    if 'animal' not in event:
-        return event['uuid'], ' deleted !!!'
+    if 'animal' not in ani:
+        return ani['uuid'], ' deleted !!!'
         
     response = table.put_item(
         Item={
-            'GGID': event['uuid'],
-            'Animal_Type' : event['animal']
+            'GGID': ani['uuid'],
+            'Animal_Type' : ani['animal']
         }
         )
-    if event['animal'] :
-        return event['uuid'], ' updated to ', event['animal'], ' !!!'
+    if ani['animal'] :
+        return ani['uuid'], ' updated to ', ani['animal'], ' !!!'
         
     '''response = table.put_item(
         Key={
